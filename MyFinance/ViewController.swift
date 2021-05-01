29e95e4d327398bbf7f68db67a949f7cce86843d
VladimirS <serbinvladimir2003@gmail.com>
2021-05-01 18:22:26 +0300

    Initial Commit

---
+++ MyFinance/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MyFinance
+//
+//  Created by Вова Сербин on 01.05.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
